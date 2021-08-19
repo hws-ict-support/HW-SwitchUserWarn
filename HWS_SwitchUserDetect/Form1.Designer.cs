0012404b66cc03d02f97a73fad1771e6af5e1b55
hws-ict-support <86113221+hws-ict-support@users.noreply.github.com>
2021-08-19 15:14:03 +0100

    Inital

---
+++ HWS_SwitchUserDetect/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace HWS_SwitchUserDetect
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnReboot = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstUsers = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(285, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 109);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(663, 354);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(102, 39);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Ok";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnReboot
+            // 
+            this.btnReboot.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnReboot.Location = new System.Drawing.Point(663, 399);
+            this.btnReboot.Name = "btnReboot";
+            this.btnReboot.Size = new System.Drawing.Size(102, 39);
+            this.btnReboot.TabIndex = 2;
+            this.btnReboot.Text = "Reboot now";
+            this.btnReboot.UseVisualStyleBackColor = false;
+            this.btnReboot.Click += new System.EventHandler(this.btnReboot_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(84, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(627, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "There is more than one user currently logged into this PC.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(734, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "You may experience issues with the sound. Please reboot to fix this.";
+            // 
+            // lstUsers
+            // 
+            this.lstUsers.FormattingEnabled = true;
+            this.lstUsers.Location = new System.Drawing.Point(104, 354);
+            this.lstUsers.Name = "lstUsers";
+            this.lstUsers.Size = new System.Drawing.Size(120, 95);
+            this.lstUsers.TabIndex = 5;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstUsers);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReboot);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Information Message - User Switching";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnReboot;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstUsers;
+    }
+}
+
---
+++ HWS_SwitchUserDetect/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Runtime.InteropServices;
+
+namespace HWS_SwitchUserDetect
+{
+    public partial class FrmMain : Form
+    {
+
+
+
+
+        [DllImport("wtsapi32.dll")]
+        static extern IntPtr WTSOpenServer([MarshalAs(UnmanagedType.LPStr)] String pServerName);
+
+        [DllImport("wtsapi32.dll")]
+        static extern void WTSCloseServer(IntPtr hServer);
+
+        [DllImport("wtsapi32.dll")]
+        static extern Int32 WTSEnumerateSessions(IntPtr hServer, [MarshalAs(UnmanagedType.U4)] Int32 Reserved, [MarshalAs(UnmanagedType.U4)] Int32 Version, ref IntPtr ppSessionInfo, [MarshalAs(UnmanagedType.U4)] ref Int32 pCount);
+
+        [DllImport("wtsapi32.dll")]
+        static extern void WTSFreeMemory(IntPtr pMemory);
+
+        [DllImport("Wtsapi32.dll")]
+        static extern bool WTSQuerySessionInformation(System.IntPtr hServer, int sessionId, WTS_INFO_CLASS wtsInfoClass, out System.IntPtr ppBuffer, out uint pBytesReturned);
+        [StructLayout(LayoutKind.Sequential)]
+        private struct WTS_SESSION_INFO
+        {
+            public Int32 SessionID;
+            [MarshalAs(UnmanagedType.LPStr)]
+            public String pWinStationName;
+            public WTS_CONNECTSTATE_CLASS State;
+        }
+
+        public enum WTS_INFO_CLASS
+        {
+            WTSInitialProgram, WTSApplicationName, WTSWorkingDirectory, WTSOEMId, WTSSessionId, WTSUserName, WTSWinStationName,
+            WTSDomainName, WTSConnectState, WTSClientBuildNumber, WTSClientName, WTSClientDirectory, WTSClientProductId,
+            WTSClientHardwareId, WTSClientAddress, WTSClientDisplay, WTSClientProtocolType
+        }
+
+        public enum WTS_CONNECTSTATE_CLASS
+        {
+            WTSActive, WTSConnected, WTSConnectQuery, WTSShadow, WTSDisconnected, WTSIdle, WTSListen, WTSReset, WTSDown, WTSInit
+        }
+
+
+        public static IntPtr OpenServer(String Name)
+        {
+            IntPtr server = WTSOpenServer(Name); return server;
+        }
+
+        public static void CloseServer(IntPtr ServerHandle)
+        {
+            WTSCloseServer(ServerHandle);
+        }
+
+        public static void ListUsers(String ServerName)
+        {
+
+        }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int loggedInUsers = 0;
+            IntPtr serverHandle = IntPtr.Zero;
+            List<String> resultList = new List<string>();
+            serverHandle = OpenServer("localhost");
+            try
+            {
+                IntPtr SessionInfoPtr = IntPtr.Zero; IntPtr userPtr = IntPtr.Zero; IntPtr domainPtr = IntPtr.Zero; Int32 sessionCount = 0;
+                Int32 retVal = WTSEnumerateSessions(serverHandle, 0, 1, ref SessionInfoPtr, ref sessionCount);
+                Int32 dataSize = System.Runtime.InteropServices.Marshal.SizeOf(typeof(WTS_SESSION_INFO));
+                Int32 currentSession = (int)SessionInfoPtr; uint bytes = 0;
+                if (retVal != 0)
+                {
+                    for (int i = 0; i < sessionCount; i++)
+                    {
+                        WTS_SESSION_INFO si = (WTS_SESSION_INFO)Marshal.PtrToStructure((System.IntPtr)currentSession, typeof(WTS_SESSION_INFO));
+                        currentSession += dataSize; WTSQuerySessionInformation(serverHandle, si.SessionID, WTS_INFO_CLASS.WTSUserName, out userPtr, out bytes);
+                        WTSQuerySessionInformation(serverHandle, si.SessionID, WTS_INFO_CLASS.WTSDomainName, out domainPtr, out bytes);
+                        Console.WriteLine("Domain and User: " + Marshal.PtrToStringAnsi(domainPtr) + "\\" + Marshal.PtrToStringAnsi(userPtr));
+                        lstUsers.Items.Add(Marshal.PtrToStringAnsi(userPtr));
+                        loggedInUsers++;
+                    }
+                    Console.ReadLine();
+                    WTSFreeMemory(SessionInfoPtr);
+                }
+            }
+            finally { CloseServer(serverHandle); }
+
+            if (loggedInUsers > 2)
+            {
+                WindowState = FormWindowState.Normal;
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnReboot_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("shutdown", "/r /t 0");
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
